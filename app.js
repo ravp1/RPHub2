4fdcb17b979df88fd281f1a4352eee0828ad9c70
ravp1 <ravp1@ymail.com>
2016-02-02 10:24:20 -0500

    categories created

--- app.js
+++ app.js
@@ -115,6 +115,173 @@ var Interest = mongoose.model('Interest', interestSchema);
 //}
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// CREATE DEFAULT CATEGORIES, INETERESTS, POSTS (ALREADY CREATED; NO NEED TO RUN EVERY TIME)
+//{ SETUP ENGINEERING CATEGORY
+var engineering = new Category({ name: 'Engineering'});
+
+engineering.interests[engineering.interests.length] = new Interest({name:'Aeronautical Engineering', nickname:"aero"});
+engineering.interests[engineering.interests.length] = new Interest({name:'Biomedical Engineering', nickname:"bmed"});
+engineering.interests[engineering.interests.length] = new Interest({name:'Chemical Engineering', nickname:"chme"});
+engineering.interests[engineering.interests.length] = new Interest({name:'Civil Engineering', nickname:"civl"});
+engineering.interests[engineering.interests.length] = new Interest({name:'Computer & Systems Engineering', nickname:"cse"});
+engineering.interests[engineering.interests.length] = new Interest({name:'Electrical Engineering', nickname:"ee"});
+engineering.interests[engineering.interests.length] = new Interest({name:'Environmental Engineering', nickname:"enve"});
+engineering.interests[engineering.interests.length] = new Interest({name:'Industrial & Management Engineering', nickname:"isye"});
+engineering.interests[engineering.interests.length] = new Interest({name:'Materials Engineering', nickname:"mtle"});
+engineering.interests[engineering.interests.length] = new Interest({name:'Mechanical Engineering', nickname:"mech"});
+engineering.interests[engineering.interests.length] = new Interest({name:'Nuclear Engineering', nickname:"nuke"});
+for (var i =0; i< engineering.interests.length; i++){
+	engineering.interests[i].noSpace = engineering.interests[i].name.replace(/ /g, "").replace(/,/g, "").replace(/&/g, "").toLowerCase();
+	engineering.interests[i].nickname = engineering.interests[i].nickname.toUpperCase();
+	engineering.interests[i].save();
+}
+console.log(engineering.interests[3]);
+engineering.save();
+//}
+
+//{ SETUP SCIENCE CATEGORY
+var science = new Category({name:'Science'});
+science.interests[science.interests.length] = new Interest({name:'Biology', nickname:"biol"});
+science.interests[science.interests.length] = new Interest({name:'Biochemistry & Biophysics', nickname:"bcbp",});
+science.interests[science.interests.length] = new Interest({name:'Bioinformatics & Molecular Biology', nickname:"bimb"});
+science.interests[science.interests.length] = new Interest({name:'Chemistry', nickname:"chem"});
+science.interests[science.interests.length] = new Interest({name:'Computer Science', nickname:"csci"});
+science.interests[science.interests.length] = new Interest({name:'Environmental Science', nickname:"envs",});
+science.interests[science.interests.length] = new Interest({name:'Geology', nickname:"geol"});
+science.interests[science.interests.length] = new Interest({name:'Hydrogeology', nickname:"hgeo"});
+science.interests[science.interests.length] = new Interest({name:'Mathematics', nickname:"math"});
+science.interests[science.interests.length] = new Interest({name:'Physics & Applied Physics', nickname:"phys" });
+for (var i =0; i< science.interests.length; i++){
+	science.interests[i].noSpace = science.interests[i].name.replace(/ /g, "").replace(/,/g, "").replace(/&/g, "").toLowerCase();
+	science.interests[i].nickname = science.interests[i].nickname.toUpperCase();
+	science.interests[i].save();
+}
+science.save();
+//}
+
+//{ SETUP MANAGEMENT CATEGORY
+var management = new Category({ name: 'Management'});
+
+management.interests[management.interests.length] = new Interest({name:'Business & Management', nickname:"mgmt"});
+management.interests[management.interests.length] = new Interest({name:'Financial Engineering', nickname:"fnce"});
+for (var i =0; i< management.interests.length; i++){
+	management.interests[i].noSpace = management.interests[i].name.replace(/ /g, "").replace(/,/g, "").replace(/&/g, "").toLowerCase();
+	management.interests[i].nickname = management.interests[i].nickname.toUpperCase();
+	management.interests[i].save();
+}
+
+management.save();
+//}
+
+//{ SETUP HASS CATEGORY
+var hass = new Category({ name: 'Humanities, Arts, & Soc. Sciences'});
+
+hass.interests[hass.interests.length] = new Interest({name:'Cognitive Science', nickname:"cogs"});
+hass.interests[hass.interests.length] = new Interest({name:'Communication', nickname:"comm"});
+hass.interests[hass.interests.length] = new Interest({name:'Design, Innovation, and Society', nickname:"dis"});
+hass.interests[hass.interests.length] = new Interest({name:'Economics', nickname:"econ"});
+hass.interests[hass.interests.length] = new Interest({name:'Electronic Arts', nickname:"ea"});
+hass.interests[hass.interests.length] = new Interest({name:'Electronic Media, Arts, & Communication', nickname:"emac"});
+hass.interests[hass.interests.length] = new Interest({name:'Games & Simulation Arts and Sciences', nickname:"gsas"});
+hass.interests[hass.interests.length] = new Interest({name:'Philosophy', nickname:"phil"});
+hass.interests[hass.interests.length] = new Interest({name:'Psychology', nickname:"psyc"});
+hass.interests[hass.interests.length] = new Interest({name:'Science, Technology & Society', nickname:"sts"});
+hass.interests[hass.interests.length] = new Interest({name:'Sustainability Studies', nickname:"sust"});
+
+for (var i =0; i< hass.interests.length; i++){
+	hass.interests[i].noSpace = hass.interests[i].name.replace(/ /g, "").replace(/,/g, "").replace(/&/g, "").toLowerCase();
+	hass.interests[i].nickname = hass.interests[i].nickname.toUpperCase();
+	hass.interests[i].save();
+}
+
+hass.save();
+
+
+//}
+
+//{ SETUP ARCHITECTURE CATEGORY
+var arch = new Category({ name: 'Architecture'});
+
+arch.interests[arch.interests.length] = new Interest({name:'Architecture', nickname:"arch"});
+arch.interests[arch.interests.length] = new Interest({name:'Lighting', nickname:"lght"});
+for (var i =0; i< arch.interests.length; i++){
+	arch.interests[i].noSpace = arch.interests[i].name.replace(/ /g, "").replace(/,/g, "").replace(/&/g, "").toLowerCase();
+	arch.interests[i].nickname = arch.interests[i].nickname.toUpperCase();
+	arch.interests[i].save();
+}
+
+arch.save();
+//}
+
+//{ SETUP IT & WS CATEGORY
+var itws = new Category({ name: 'Information Technology & Web Science'});
+
+itws.interests[itws.interests.length] = new Interest({name:'Information Technology & Web Science', nickname:"itws"});
+for (var i =0; i< itws.interests.length; i++){
+	itws.interests[i].noSpace = itws.interests[i].name.replace(/ /g, "").replace(/,/g, "").replace(/&/g, "").toLowerCase();
+	itws.interests[i].nickname = itws.interests[i].nickname.toUpperCase();
+	itws.interests[i].save();
+}
+
+itws.save();
+//}
+
+var timeStamp = new Date();
+var timeStr = timeStamp.toDateString();
+var posts = [];
+var post1 = new Post({poster:'Sample Professor', content:'For students looking for experience with digital design. For credit or salary.', title:'VLSI Project', tags:['Electrical Engineering'], fullfilled:false, replyAddress: 'panser@rpi.edu', timePosted: timeStamp, timeString: timeStr});
+post1.save();
+posts.push(post1);
+var post2 = new Post({poster:'Sample Professor', content:"Computational physics research in density functional theory. Must be junior or above with programming experience.", title:'DFT Research', fulfilled:false, replyAddress: 'panser@rpi.edu', timePosted: timeStamp, timeString: timeStr});
+post2.save();
+posts.push(post2);
+var post3 = new Post({poster:'Sample Professor', content:"More physics stuff find out more later.", title:'Physics', fulfilled:true, replyAddress: 'panser@rpi.edu', timePosted: timeStamp, timeString: timeStr});
+post3.save();
+posts.push(post3);
+
+
+//}
+//console.log("engineering: " + JSON.stringify(engineering));
+//console.log("first interest posts: " + Interest.findOne().exec().returnPosts());
+
+Interest.findOne({name: "Electrical Engineering"},function(err,result){
+	console.log("err is " + err);
+	//console.log("first interest posts: " + JSON.stringify(result.returnPosts() ) );
+	result.returnPosts()
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Find all categories and store them in a variable
 //so they can be freely referenced later without
 //having to find them all first
